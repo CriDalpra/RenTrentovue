5b78ba1b9936735b1026dc6afd1afeab613f798c
Cristian Dalpra <cristian.dalpra@studenti.unitn.it>
2026-02-17 17:22:27 +0100

    Setup iniziale

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="hero min-h-[50vh] bg-base-100 rounded-box shadow-xl mt-10">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Benvenuto!</h1>
+        <p class="py-6">Se leggi questo, Vue + Tailwind + DaisyUI funzionano correttamente.</p>
+        <button class="btn btn-primary">Bottone di Test</button>
+      </div>
+    </div>
+  </div>
 </template>
